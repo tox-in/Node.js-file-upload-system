bd91d02bd05fb5ec72a41f7b09d871889ce09d87
tox-in <habimanat78@gmail.com>
2024-02-02 12:06:22 +0200

    necessary changes in index.ts are made

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express, { type Request, type Response } from 'express';
+import dotenv from 'dotenv';
+import { uploadMulter, uploadB2 } from './middleware';
+
+dotenv.config();
+const app = express();
+
+//routes
+app.post('/upload', uploadMulter ,uploadB2, (req: Request, res: Response) => {
+    res.json(res.locals);
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`running at ${process.env.PORT}`);
+});
